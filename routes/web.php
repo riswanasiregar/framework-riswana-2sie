ce0e8e126c4494663e533318b01825c314ec4fe5
riswanasiregar <riswana24si@mahasiswa.pcr.ac.id>
2025-09-24 08:07:12 +0700

    Starter Projek Laravel

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/pcr', function () {
+    return 'Selamat Datang di Website Kampus PCR!';
+});
+Route::get('/nama/{riswana}', function ($riswana) {
+    return 'Nama saya: '.$riswana;
+});
+Route::get('/nim/{param1?}', function ($param1 = '') {
+    return 'NIM saya: '.$param1;
+});
+Route::get('/nama/{param1?}/{nim?}', function ($param1 = '', $nim ='') {
+    return 'Nama saya: '.$param1.'<br> NIM saya: '.$nim;
+});
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class,'show'] )->name('mahasiswa.show');
+Route::get('/nama/{param1?}/{nim?}', function ($param1='',$nim='') {
+    return 'Nama saya : '.$param1.'<br>Nim : '.$nim;
+});
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+
+
+
